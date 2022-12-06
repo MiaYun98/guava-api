df18881e068fe27714c30254f1b1981e39a4ba4d
Mia <113086524+MiaYun98@users.noreply.github.com>
2022-12-06 11:29:32 -0800

    linking the tag

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,14 @@ const userRoutes = require('./user-routes');
 const profileRoutes = require('./profile-routes');
 const shrubsRoutes = require('./shrub-routes');
 const itemRoutes = require('./Item-routes');
+const profileTagRoutes = require('./profiletag-routes');
+const shrubTagRoutes = require('./shrubtag-routes');
 
 router.use('/user', userRoutes);
 router.use('/profile', profileRoutes);
 router.use('/shrub', shrubsRoutes);
-router.use('/Item', shrubsRoutes);
-
+router.use('/Item', itemRoutes);
+router.use('./profiletag', profileTagRoutes);
+router.use('./shrubtag', shrubTagRoutes);
 
 module.exports = router;
