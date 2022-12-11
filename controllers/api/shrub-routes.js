e1a764ae03a3019d876fb2d38127515c0987f5d4
Sangmi <113086524+MiaYun98@users.noreply.github.com>
2022-12-10 23:41:19 -0800

    Update shrub-routes.js

--- controllers/api/shrub-routes.js
+++ controllers/api/shrub-routes.js
@@ -51,7 +51,7 @@ router.put("/update", (req, res) => {
 
 router.post('/create', (req, res) => {
     Shrub.create({
-        name: "Shrub",
+        name: req.body.name,
         level: 1,
         hunger: 100,
         hygiene: 100,
